d43cfa4952468fb88f7275c9c7c29e4cb7ba0800
Wizardy <wizardhassan99@gmail.com>
2024-03-16 11:44:17 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
-import { useState } from 'react';
 import './App.css'
+import UseState from './Hooks/useEffects/UseState'
 
 function App() {
-
-  const [items, setItems] = useState(list);
-
-  const list = [
-    {id:1, name:"test 1"},
-    {id:2, name:"test 2"},
-    {id:3, name:"test 3"},
-    {id:4, name:"test 4"},
-    {id:5, name:"test 5"},
-  ];
-  const updateList = () =>{
-    const updatedList = [...items];
-
-  }
   return (
     <>
-      <div className="list-items">
-        <ul>
-          {
-            list.map( l => <li>{l.name}</li>)
-          }
-        </ul>
-        <button>Remove element</button>
-      </div>
+      <UseState/>
     </>
   )
 }
