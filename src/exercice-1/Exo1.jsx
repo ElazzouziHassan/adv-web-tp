5da15ab6fcee39459cdfb6be685dc0f4ed19bd42
Wizardy <wizardhassan99@gmail.com>
2024-03-16 14:23:26 +0000

    commit

---
+++ src/exercice-1/Exo1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Exo1() {
+  return (
+    <div className="exo-1">
+      <form action="">
+        <label htmlFor="nom">Nom :</label><br />
+        <input type="text" name="nom" id="nom" /><br />
+        <label htmlFor="email">Email :</label><br />
+        <input type="text" name="nom" id="nom" /><br />
+      </form>
+    </div>
+  )
+}
+
+export default Exo1
