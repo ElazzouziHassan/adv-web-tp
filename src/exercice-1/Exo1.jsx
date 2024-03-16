4d9dc344e66b36e828c15eecc50b786874dbf334
Wizardy <wizardhassan99@gmail.com>
2024-03-16 15:00:53 +0000

    commit

--- src/exercice-1/Exo1.jsx
+++ src/exercice-1/Exo1.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 function Exo1() {
+
+  const [data, setData] = useState({
+    name: '',
+    email: ''
+  });
+
+  // const emailRef = useRef();
+  // const nomRef = useRef();
+  
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Nom:', data.name);
+    console.log('Email:', data.email);
+  };
+
   return (
-    <div className="exo-1">
-      <form action="">
-        <label htmlFor="nom">Nom :</label><br />
-        <input type="text" name="nom" id="nom" /><br />
-        <label htmlFor="email">Email :</label><br />
-        <input type="text" name="nom" id="nom" /><br />
+    <div className="exo-1 sm:flex sm:flex-col sm:items-center sm:justify-center sm:space-y-4">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="nom">Nom :</label>
+          <input 
+            // ref={nomRef} 
+            type="text" 
+            name="nom" 
+            value={data.name}  
+            onChange={handleInputChange} 
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email :</label>
+          <input 
+            // ref={emailRef}
+            type="email" 
+            name="nom" 
+            value={data.email}
+            onChange={handleInputChange} 
+            // required
+          />
+        </div>
+        <button type='submit'>Submit</button>
       </form>
     </div>
   )
